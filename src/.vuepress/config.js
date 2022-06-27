0f54d4bd916a62f8d6a1cd52fac6934a98a2492d
荻窪りょう <wowane.info@gmail.com>
2022-06-27 13:01:13 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -19,8 +19,7 @@ module.exports = {
     ["link", { rel: "icon", href: "/favicon.ico" }],
     ['meta', { name: 'theme-color', content: '#1e2554' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]]
   ],
 
   /**
